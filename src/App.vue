c49e74b8bbfd20f1b13aa4be5d446434f8bf3448
rifqiiardhian <rifqiardhian@gmail.com>
2020-03-19 18:09:51 +0700

    uploads

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div>
+            <b-navbar class="navbar" id="nav" toggleable="lg" fixed="top">
+                <b-navbar-brand  class="navbar-brand" href="#home">
+                    <img src="./assets/images/logo.png" alt="logo" class="logo">
+                </b-navbar-brand>
+
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                <b-collapse id="nav-collapse" class="nav-collapse" is-nav>
+                    <b-navbar-nav class="ml-auto">
+                        <b-nav-item class="nav-item" href="#home" right>Home</b-nav-item>
+                        <b-nav-item class="nav-item" href="#profile" right>Profile</b-nav-item>
+                        <b-nav-item class="nav-item" href="#services" right>Services</b-nav-item>
+                        <b-nav-item class="nav-item" href="#education" right>Education</b-nav-item>
+                        <b-nav-item class="nav-item" href="#experiences" right>Experiences
+                        </b-nav-item>
+                        <b-nav-item class="nav-item" href="#portfolio" right>Portfolio</b-nav-item>
+                        <b-nav-item class="nav-item" href="#contact" right>Contact</b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-navbar>
+        </div>
+        <router-view />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener('scroll', () => {
+        const navbar = document.getElementById('nav');
+        // eslint-disable-next-line camelcase
+        const nav_class = navbar.classList;
+        if (document.documentElement.scrollTop >= 150) {
+          if (nav_class.contains('shrink') === false) {
+            nav_class.toggle('shrink');
+          }
+        } else if (nav_class.contains('shrink') === true) {
+          nav_class.toggle('shrink');
+        }
+      });
+    });
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import 'css/style.css';
 </style>
---
+++ src/views/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="home">
+        <div class="banner" id="home">
+            <img src="../assets/images/banner.png" class="img-banner img-fluid">
+            <h1 class="title">HOUSE OF TIME</h1>
+            <h4 class="subtitle">Official Webpage</h4>
+        </div>
+        <section class="product">
+            <div class="container-fluid container-product">
+                <div class="row row-product">
+                    <div class="col-md-3" v-for="(data, index) in products" :key="index">
+                        <div class="product-item">
+                            <div class="product-img text-center">
+            <img :src="require(`../assets/images/${data.foto}`)" class="img-product"/>
+                            </div>
+                            <div class="product-detail text-center">
+                                <h5><b>{{data.merk}}</b></h5>
+                                <h6>{{data.harga}}</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  async created() {
+    const BASEURI = 'https://my-json-server.typicode.com/rifqiiardhian/uts/db';
+    axios.get(BASEURI).then((result) => {
+      this.products = result.data.products;
+    });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import '../css/style.css';
+</style>
