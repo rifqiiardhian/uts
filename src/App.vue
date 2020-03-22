63eac83960ca3badb78fe29d59f1cdd0f2603ade
rifqiiardhian <rifqiardhian@gmail.com>
2020-03-22 11:38:58 +0700

    upload2

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
+// ---------------------------------------
+// Copyright 2020
+// Created and Designed by Rifqi Ardhian
+// Originally Stylling
+// User Interface Mid Test
+// ---------------------------------------
+
 <template>
     <div id="app">
+        <link rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+            integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+            crossorigin="anonymous">
         <div>
             <b-navbar class="navbar" id="nav" toggleable="lg" fixed="top">
-                <b-navbar-brand  class="navbar-brand" href="#home">
-                    <img src="./assets/images/logo.png" alt="logo" class="logo">
+                <b-navbar-brand  class="navbar-brand m-auto" href="#home">
+                    <img src="./assets/images/logojam.png" alt="logo" class="logo">
                 </b-navbar-brand>
-
                 <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
+                <b-collapse id="nav-collapse" class="nav-collapse" is-nav>
+                    <b-navbar-nav class="mr-auto">
+                        <b-nav-item class="nav-item" href="" right>Home</b-nav-item>
+                        <b-nav-item class="nav-item" href="" right>About Us</b-nav-item>
+                        <b-nav-item class="nav-item" href="" right>Men Watch</b-nav-item>
+                        <b-nav-item class="nav-item" href="" right>Women Watch
+                        </b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
                 <b-collapse id="nav-collapse" class="nav-collapse" is-nav>
                     <b-navbar-nav class="ml-auto">
-                        <b-nav-item class="nav-item" href="#home" right>Home</b-nav-item>
-                        <b-nav-item class="nav-item" href="#profile" right>Profile</b-nav-item>
-                        <b-nav-item class="nav-item" href="#services" right>Services</b-nav-item>
-                        <b-nav-item class="nav-item" href="#education" right>Education</b-nav-item>
-                        <b-nav-item class="nav-item" href="#experiences" right>Experiences
+                        <b-nav-item class="nav-item" href="" right>
+                            <i class="fas fa-search"></i>
                         </b-nav-item>
-                        <b-nav-item class="nav-item" href="#portfolio" right>Portfolio</b-nav-item>
-                        <b-nav-item class="nav-item" href="#contact" right>Contact</b-nav-item>
+                        <b-nav-item class="nav-item" href="" right>
+                            <i class="fas fa-user"></i>
+                        </b-nav-item>
+                        <b-nav-item class="nav-item" href="" right>
+                            <i class="fas fa-shopping-cart"></i>
+                        </b-nav-item>
                     </b-navbar-nav>
                 </b-collapse>
             </b-navbar>
@@ -48,5 +67,5 @@ export default {
 </script>
 
 <style>
-@import 'css/style.css';
+@import 'css/app.css';
 </style>
